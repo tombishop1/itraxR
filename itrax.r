eb19b938be20f238d1f367f59c6722b4cdf0ecfa
Gerard Capes <gerard.capes@manchester.ac.uk>
2016-12-05 10:32:11 +0000

    Add debugging lines for #2

--- itrax.r
+++ itrax.r
@@ -58,8 +58,22 @@ ai <- as.numeric(ai)
 # perform the binning operation
 df_avg <- aggregate(df,list(rep(1:(length(df$depth)%/%ai+1),each=ai,len=length(df$depth))),mean)[-1]
 df_avg$depthmin <- aggregate(df$depth,list(rep(1:(length(df$depth)%/%ai+1),each=ai,len=length(df$depth))),min)[-1]
+
+# debug lines
+print("head(df_avg)=")
+print(head(df_avg))
+print("names(df_avg)=")
+print(names(df_avg))
+
 names(df_avg)[names(df_avg)=="x"] <- "depth_min" # this doesn't work properly. Weird.
 df_avg$depthmax <- aggregate(df$depth,list(rep(1:(length(df$depth)%/%ai+1),each=ai,len=length(df$depth))),max)[-1]
+
+# debug lines
+print("head(df_avg)=")
+print(head(df_avg))
+print("names(df_avg)=")
+print(names(df_avg))
+
 names(df_avg)[names(df_avg)=="x"] <- "depth_max" # this doesn't work properly. Weird.
 
 # perform some basic multi-variate analysis
