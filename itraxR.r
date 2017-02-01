ef1bc8c973395320063268d56af08fa3156cf6a4
tombishop1 <tombishopemail@gmail.com>
2017-02-01 16:35:45 +0000

    Improve Robustness of Parsing
    Improve the robustness and error messages in the parsing functions,
    closes #14

--- itraxR.r
+++ itraxR.r
@@ -4,22 +4,23 @@
 ## ITRAX-IMPORT  ##
 ###################
 
-itrax_import=function(datafile="result.txt", depth_top=NULL, trim_top=NULL, trim_bottom=NULL, na_validity=NULL, few_parameters=TRUE, graph=NULL, export=NULL) {
-# datafile			= 	name of the input data
-# depth_top			= 	defines the coring depth of the top of the scan
-# trim_top			= 	how much bad data at the start of the scan to remove
-# trim_bottom		= 	how much bad data at the end of the scan to remove
-# na_validity		= 	makes invalid data "NA" - on by default
-# few_parameters	= 	TRUE (default) passes elemental and limited metadata
-#						FALSE passes all data without removing anything
-# graph				=	draws a graph
-# export			=	exports the data
+itrax_import=function(datafile="result.txt", depth_top=NULL, trim_top=NULL, trim_bottom=NULL, na_validity=TRUE, 
+                      parameters="SOME", graph=FALSE, export=NULL) {
+  
+# datafile    pass the name of the datafile you want to parse.
+# depth_top   pass he coring depth of the sequence in mm.
+# trim_top    pass how much of the top of the core to remove in mm. combine with negative depth_top if required.
+# trim_bottom pass how much of the bottom of the core to remove in mm.
+# na_validity sets all invalid measurements to NA.
+# parameters  use ALL, SOME or ELEMENTS to select which data to include. 
+# graph       TRUE or FALSE to plot a stratigraphic diagram. 
+# export      pass the name of file you want to export the data to, if you want to export.
+
 
 # assert file exists
 if (!file.exists(datafile)){
   stop('File doesn\'t exist, check your working directory.')
-  return()
-}
+} else{}
 
 # read the result file into a dataframe
 df = read.table(datafile, skip = 2, header = TRUE, sep = "\t")
@@ -44,14 +45,14 @@ elements = c("H", "He",
 others = c("position..mm.", "sample.surface", "MSE", "cps", "validity")
 
 # remove parameters that aren't of interest
-if(few_parameters==TRUE){
+if(parameters=="SOME"){
 	df <- df[ , which(names(df) %in% append(elements, others))]
-} else if(few_parameters==FALSE){
+} else if(parameters=="ALL"){
 	df <- df
-# } else if(few_parameters=="ELEMENTS"){
-#	df <- df[ , which(names(df) %in% elements)]
-#} else {
-#	df <- df[ , which(names(df) %in% few_parameters)]
+} else if(parameters=="ELEMENTS"){
+  df <- df[ , which(names(df) %in% append(elements, append(others[[1]], others[[5]])))]
+} else {
+warning('The parameters variable must be SOME, ALL or ELEMENTS.')
 }
 
 # re-name the position variable
@@ -65,38 +66,71 @@ if(!is.null(depth_top)){
 	depth_offset = depth_top - df[1, "position"]
 	df$depth = df$position + depth_offset
 	rownames(df) <- round(df$depth, digits = 1)
+} else{}
+
+# trim the junk data at the beginning of the scan
+
+# establish whether depth or position is being used
+if(!is.null(depth_top)){
+  x <- df$depth
+} else{x <- df$position}
+
+if(!is.null(trim_top)){
+	top_trim_position <- min(x) + trim_top
+	df <- subset(df, x > top_trim_position)
+} 
+
+# trim the junk data at the end of the scan
+if(!is.null(trim_bottom)){
+	bottom_trim_position <- max(x) - trim_bottom 
+	df <- subset(df, x < bottom_trim_position)
 }
 
 # Replace all rows with validity == 0 with NA
-if(!is.null(na_validity)){
-	df[df$validity == FALSE , ] <- NA
-	df$validity[is.na(df$validity)] <- 0 
+if(na_validity==TRUE){
+  
+  position_temp <- df$position
+  
+  if(!is.null(depth_top)){
+    depth_temp <- df$depth
+  } else{}
+  
+  df[df$validity == FALSE , ] <- NA
+  
+  df$position <- position_temp
+  
+  if(!is.null(depth_top)){
+    df$depth <- depth_temp
+  }
+} else if(na_validity==FALSE){
+} else{
+  stop('The na_validity parameter must be TRUE or FALSE.')
 }
 
 # return the values as logic
 df$validity = as.logical(df$validity)
 
-# trim the junk data at the beginning of the scan
-if(!is.null(trim_top)){
-	top_trim_position <- min(df$depth) + trim_top
-	df <- subset(df, df$depth > top_trim_position)
-} 
-
-# trim the junk data at the end of the scan
-if(!is.null(trim_bottom)){
-	bottom_trim_position <- max(df$depth) - trim_bottom 
-	df <- subset(df, df$depth < bottom_trim_position)
-}
-
 # make a pretty graph
-if(!is.null(graph)){
+
+if(!is.null(depth_top)){
+  ylabel <- "Depth (mm)"
+}else{ylabel <- "Position (mm)"}
+
+if(graph==TRUE){
 	require(analogue)
-	Stratiplot(df[-grep("depth|position",colnames(df))], df$depth, varTypes = "absolute", absolutesize = "1", strip = TRUE)
+    if(!is.null(depth_top)){ Stratiplot(df[-grep("depth|position",colnames(df))], df$depth, varTypes = "absolute", 
+                             absolutesize = "1", strip = TRUE, ylab=as.character(ylabel)) }
+    else{	Stratiplot(df[-grep("depth|position",colnames(df))], df$position, varTypes = "absolute", 
+  	      absolutesize = "1", strip = TRUE, ylab=as.character(ylabel)) }
+} else if(graph==FALSE){
+} else{
+  stop('The graph parameter must be TRUE or FALSE.')
 }
 
 # export the data
-if(!is.null(export))
+if(!is.null(export)){
 	write.table(df, file = export, sep = "\t")
+} else{}
 
 # return the data
 return(df)
@@ -106,23 +140,30 @@ return(df)
 ## ITRAX-ORDINATION ##
 ######################
 
+# dataframe     pass the name of a dataframe parsed using itrax_import
+# elementsonly  TRUE or FALSE - false passes all data, TRUE only elements
+# zeros         addone adds one to everything, limit replaces zeros with 0.001
+# transform     perform centered log transform, or no transformation
+# diagrams      draw some diagrams, or not
+
 itrax_ordination=function(dataframe, elementsonly=TRUE, zeros="addone", transform=TRUE, diagrams=TRUE) {
 
 # check the dataframe exists
 if(is.data.frame(dataframe)){
-# if(exists(dataframe) && is.data.frame(get(dataframe))){
 	df <- dataframe
-} #else {
-#	stop('Dataframe does not exist or object is not a dataframe.')
-#	return()
-#}
+} else{
+stop('Dataframe does not exist or object is not a dataframe.')
+}
 
 # rename the rows
-#if("depth" %in% colnames(df)) {
-#	rownames(df) <- round(df$depth)
-#} else if("position" %in% colnames(df)) {
-#	rownames(df) <- round(df$position)
-#} 
+if("depth" %in% colnames(df)) {
+	rownames(df) <- round(df$depth)
+	labels <- "Depth (mm)"
+} else{
+  df$position <- as.numeric(df$position)
+	rownames(df) <- df$position
+	labels <- "Position (mm)"
+} 
 
 # get rid of anything that isn't an element
 elements = c("H", "He",
@@ -140,7 +181,9 @@ elements = c("H", "He",
 
 if(elementsonly==TRUE) {
 	df <- df[ , which(names(df) %in% elements)]
+} else if(elements==FALSE){
 } else{
+  stop('elements only must be true or false')
 }
 
 # deal with zero values
@@ -148,27 +191,37 @@ if(zeros=="addone") {
 	df <- df + 1
 } else if(zeros=="limit") {
 	df[df == 0] <- 0.001
+}else{
+  warning('zero values may be present because zeros is not valid')
 }  
 
 # calculate centered log ratios for all elements in the original dataset
 if(transform==TRUE) {
 	require(chemometrics) # could also use "compositions" package
 	df <- clr(df)
+} else if(transform==FALSE) {
+} else{
+  stop('transform must be TRUE or FALSE')
 }
 
 # perform a principle components analysis of the original dataset and display the eigenvalues
-df_pca <- prcomp(df)
-print(df_pca$rotation)
+df_pca <- prcomp(na.omit(df))
+print(summary(df_pca$rotation))
 
 # plot an ordination diagram of axes 1 and 2, with sample and variable scores, to explore the data
 # then plot the first axis by position
 # and also plot the axis one loadings
 if(diagrams==TRUE) {
-	biplot(df_pca)
 	dev.new()
-	plot(row.names(df_pca$x), df_pca$x[ , 1])
+  biplot(df_pca)
 	dev.new()
-	barplot(sort(df_pca$rotation[ , "PC1"]), horiz=TRUE, names.arg=row.names(sort(df_pca$rotation[ , "PC1"])), cex.names=0.6)
+	plot(row.names(df_pca$x), df_pca$x[ , 1], xlab=labels, ylab="PC1")
+	dev.new()
+	barplot(sort(df_pca$rotation[ , "PC1"]), horiz=TRUE, names.arg=row.names(sort(df_pca$rotation[ , "PC1"])), 
+	        cex.names=0.6, xlab="PC1")
+} else if(diagrams==FALSE){
+} else{
+  stop('diagrams must be TRUE or FALSE')
 }
 
 return(df_pca)
@@ -179,62 +232,75 @@ return(df_pca)
 #######################
 
 itrax_correlation=function(dataframe, elementsonly=TRUE, zeros="addone", transform=TRUE, diagrams=TRUE) {
-require(corrplot)
+
 # check the dataframe exists
 if(is.data.frame(dataframe)){
- #if(exists(dataframe) && is.data.frame(get(dataframe))){
-	df <- dataframe
-} #else {
-#	stop('Dataframe does not exist or object is not a dataframe.')
-#	return()
-#}
-
+  df <- dataframe
+} else{
+  stop('Dataframe does not exist or object is not a dataframe.')
+}
+  
 # rename the rows
-#if("depth" %in% colnames(df)) {
-#	rownames(df) <- round(df$depth)
-#} else if("position" %in% colnames(df)) {
-#	rownames(df) <- round(df$position)
-#} 
-
+if("depth" %in% colnames(df)) {
+  rownames(df) <- round(df$depth)
+  labels <- "Depth (mm)"
+} else{
+  df$position <- as.numeric(df$position)
+  rownames(df) <- df$position
+  labels <- "Position (mm)"
+} 
+  
 # get rid of anything that isn't an element
 elements = c("H", "He",
-"Li", "Be", "B", "C", "N", "O", "F", "Ne",
-"Na", "Mg", "Al", "Si", "P", "S", "Cl", 
-# "Ar",
-"K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
-"Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe",
-"Cs", "Ba", 
-"La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", 
-"Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
-"Fr", "Ra", 
-"Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", 
-"Lr", "Unq", "Unp", "Unh", "Uns", "Uno", "Une", "Unn")
-
+             "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+             "Na", "Mg", "Al", "Si", "P", "S", "Cl", 
+             # "Ar",
+             "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
+             "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe",
+             "Cs", "Ba", 
+             "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", 
+             "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
+             "Fr", "Ra", 
+             "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", 
+             "Lr", "Unq", "Unp", "Unh", "Uns", "Uno", "Une", "Unn")
+  
 if(elementsonly==TRUE) {
-	df <- df[ , which(names(df) %in% elements)]
+  df <- df[ , which(names(df) %in% elements)]
+} else if(elements==FALSE){
 } else{
+  stop('elements only must be true or false')
 }
-
+  
 # deal with zero values
 if(zeros=="addone") {
-	df <- df + 1
+    df <- df + 1
 } else if(zeros=="limit") {
-	df[df == 0] <- 0.001
+    df[df == 0] <- 0.001
+}else{
+warning('zero values may be present because zeros is not valid')
 }  
-
+  
 # calculate centered log ratios for all elements in the original dataset
 if(transform==TRUE) {
-	require(chemometrics) # could also use "compositions" package
-	df <- clr(df)
+  require(chemometrics) # could also use "compositions" package
+  df <- clr(df)
+} else if(transform==FALSE) {
+} else{
+    stop('transform must be TRUE or FALSE')
 }
 
 # run the correlation matrix
+# in time, I'll add confidence levels to this
 df_cor <- cor(df, use = "pairwise.complete.obs", method = "pearson")
 
 # run diagrams
 if(diagrams==TRUE) {
-	dev.new()
-	corrplot.mixed(df_cor, lower="number", upper="color", order="AOE" )
+  require(corrplot)
+  dev.new()
+	corrplot.mixed(df_cor, lower="number", upper="color", order="AOE", number.cex=0.75 ) # and confidence levels to this
+} else if(diagrams==FALSE){
+} else{
+  stop('diagrams must be TRUE or FALSE')
 }
 
 return(df_cor)
@@ -246,8 +312,26 @@ return(df_cor)
 #######################
 
 itrax_averaging=function(dataframe, interval) {
+  
+# dataframe     pass the name of a dataframe parsed using itrax_import
+# interval      pass the size of the interval in mm
 
-df <- dataframe
+# assert the dataframe exists and import it
+if(is.data.frame(dataframe)){
+  df <- dataframe
+} else{
+  stop('Dataframe does not exist or object is not a dataframe.')
+}
+
+# check if depths are present and subsistute position if not
+if("depth" %in% colnames(df)) {
+  position_subs <- FALSE
+} else if("position" %in% colnames(df)){
+  df$depth <- df$position
+  position_subs <- TRUE
+} else{
+  stop('Neither depth nor columns are present. Did you parse you data in itrax_import?')
+}
 
 # work out the distance as row on row increment represents
 scaninterval <- df[2, "depth"] - df[1, "depth"]
@@ -263,6 +347,17 @@ df_avg          <- aggregate(df,list(rep(1:(length(df$depth)%/%interval+1),each=
 df_avg$depthmin <- aggregate(df$depth,list(rep(1:(length(df$depth)%/%interval+1),each=interval,len=length(df$depth))),min)$x
 df_avg$depthmax <- aggregate(df$depth,list(rep(1:(length(df$depth)%/%interval+1),each=interval,len=length(df$depth))),max)$x
 
+# rename the columns if they need it
+if(position_subs==TRUE){
+  df_avg$position <- df_avg$depth
+  df_avg$positionmin <- df_avg$depthmin
+  df_avg$positionmax <- df_avg$depthmax
+  df_avg$depth <- NULL
+  df_avg$depthmin <- NULL
+  df_avg$depthmax <- NULL
+}else if(position_subs==FALSE){
+}
+
 return(df_avg)
 }
 
@@ -270,14 +365,15 @@ return(df_avg)
 ##    ITRAX-JOIN     ##
 #######################
 
+# DEPTH MUST BE PRESENT
 # list should be in the format "list(core1 = dataframe1, core 2 = dataframe2, etc...)"
 
 # label the individual scans - this needs to iterate along the list called "list", e.g.
 # dataframe1$labels <- deparse(substitute(dataframe1))
 # dataframe2$labels <- deparse(substitute(dataframe2)) etc. etc.
 
-
 itrax_join=function(list){
+  
 require(dplyr)
 
 # label the data
@@ -296,9 +392,14 @@ return(df)
 ##    ITRAX-META     ##
 #######################
 
-# Function takes a document.txt file and parses it into a sensible format. 
+# Function takes a document.txt file and parses it into a sensible format.
 
 itrax_meta=function(datafile="document.txt"){
+
+# assert file exists
+if (!file.exists(datafile)){
+  stop('File doesn\'t exist, check your working directory.')
+} else{}
 
 # import the file
 paths <- read.table(datafile, header = TRUE, sep = "\t", nrows = 1)
