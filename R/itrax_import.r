81248a31693a5e614aa1b46ea88e775d118d1f21
tombishop1 <tombishopemail@gmail.com>
2020-11-09 15:29:43 +0000

    Rewrite of itrax_import
    The old itrax_import function had some function bloat. This is a cleaner version, written in the tidyverse style, with better error messaging.

--- R/itrax_import.r
+++ R/itrax_import.r
@@ -6,148 +6,77 @@
 #' @param depth_top defines the coring in depth of the top of the core, in mm
 #' @param trim_top defines the length of any trimming required of data at the top of the core, in mm
 #' @param trim_bottom defines the length of any trimming required at the bottom of the core, in mm
-#' @param na_validity binary operator that defines whether to set all invalid data to NA
-#' @param parameters one of ALL (leave all parameters), SOME (remove some less useful parameters), or ELEMENTS to leave only elemental data
-#' @param graph binary operator that defines whether a graphical output should be produced
-#' @param export defines the name of a text file if an output file should be produced
+#' @param parameters one of all (leave all parameters), some (remove some less useful parameters)
 #'
-#' @return a dataframe of the parsed Itrax data
-#'
-#' @examples
-#' itrax_import("result.txt"), depth_top=0, trim_top=5, trim_bottom=5, graph=FALSE)
+#' @return a tibble of the parsed Itrax data
 #'
 #' @export
 
-# BUG! If the trims ae different, there is a fatal error
 
-itrax_import=function(datafile="result.txt", depth_top=NULL, trim_top=NULL, trim_bottom=NULL, na_validity=TRUE,
-                      parameters="SOME", graph=FALSE, export=NULL) {
+itrax_import <- function(filename = "Results.txt", depth_top = NA, trim_top = 0, trim_bottom = 0, parameters = "some"){
 
-  # datafile    pass the name of the datafile you want to parse.
-  # depth_top   pass he coring depth of the sequence in mm.
-  # trim_top    pass how much of the top of the core to remove in mm. combine with negative depth_top if required.
-  # trim_bottom pass how much of the bottom of the core to remove in mm.
-  # na_validity sets all invalid measurements to NA.
-  # parameters  use ALL, SOME or ELEMENTS to select which data to include.
-  # graph       TRUE or FALSE to plot a stratigraphic diagram.
-  # export      pass the name of file you want to export the data to, if you want to export.
+  require(tidyverse)
+  require(janitor)
 
+  elements <- c( "H", "He",
+                 "Li", "Be", "B" , "C" , "N", "O", "F" , "Ne",
+                 "Na", "Mg", "Al", "Si", "P", "S", "Cl", # "Ar",
+                 "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
+                 "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe",
+                 "Cs", "Ba",
+                 "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+                 "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
+                 "Fr", "Ra",
+                 "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No",
+                 "Lr", "Unq", "Unp", "Unh", "Uns", "Uno", "Une", "Unn" )
+  others <- c( "position (mm)", "sample.surface", "MSE", "cps", "validity", "Mo inc", "Mo coh", "Cr inc", "Cr coh" )
 
-  # assert file exists
-  if (!file.exists(datafile)){
-    stop('File doesn\'t exist, check your working directory.')
-  } else{}
+  # import and tidy
+  df <- suppressMessages(suppressWarnings(read_tsv(filename, skip = 2))) %>%
+    remove_empty(which = c("rows", "cols")) %>%
+    mutate(validity = as.logical(validity))
 
-  # read the result file into a dataframe
-  df = read.table(datafile, skip = 2, header = TRUE, sep = "\t")
+  # remove stuff we don't need
+  if(parameters == "some"){
+    df <- df %>% select(any_of(c(elements, others)))
+  } else if(parameters == "all"){
+  } else{stop("parameters must be some or all.")}
 
-  # select only variables of interest
-  # first, generate some lists
-  # a list of element names
-  elements = c("H", "He",
-               "Li", "Be", "B", "C", "N", "O", "F", "Ne",
-               "Na", "Mg", "Al", "Si", "P", "S", "Cl",
-               # "Ar",
-               "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
-               "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe",
-               "Cs", "Ba",
-               "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
-               "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
-               "Fr", "Ra",
-               "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No",
-               "Lr", "Unq", "Unp", "Unh", "Uns", "Uno", "Une", "Unn")
+  # sort out the position and depth
+  if(is.numeric(depth_top) == TRUE){
+    df <- df %>% rename(position = `position (mm)`) %>%
+      mutate(depth = position - min(position) + depth_top) %>%
+      select(depth, everything())
+  } else if(!is.na(depth_top)){
+    stop("depth_top must be numeric or NA.")
+  } else{df <- df %>% rename(position = `position (mm)`)}
 
-  # a list of other useful parameters
-  others = c("position..mm.", "sample.surface", "MSE", "cps", "validity", "Mo.inc", "Mo.coh", "Cr.inc", "Cr.coh")
-
-  # remove parameters that aren't of interest
-  if(parameters=="SOME"){
-    df <- df[ , which(names(df) %in% append(elements, others))]
-  } else if(parameters=="ALL"){
-    df <- df
-  } else if(parameters=="ELEMENTS"){
-    df <- df[ , which(names(df) %in% append(elements, append(others[[1]], others[[5]])))]
-  } else {
-    warning('The parameters variable must be SOME, ALL or ELEMENTS.')
-  }
-
-  # re-name the position variable
-  colnames(df)[which(names(df) == "position..mm.")] <- "position"
-
-  # convert position into an numeric value
-  df$position = as.numeric(df$position)
-
-  # calculate the new depth variable
-  if(!is.null(depth_top)){
-    depth_offset = depth_top - df[1, "position"]
-    df$depth = df$position + depth_offset
-    rownames(df) <- round(df$depth, digits = 1)
-  } else{}
-
-  # trim the junk data at the beginning of the scan
-
-  # establish whether depth or position is being used
-  if(!is.null(depth_top)){
-    x <- df$depth
-  } else{x <- df$position}
-
-  if(!is.null(trim_top)){
-    top_trim_position <- min(x) + trim_top
-    df <- subset(df, x > top_trim_position)
-  }
-
-  # trim the junk data at the end of the scan
-  if(!is.null(trim_bottom)){
-    bottom_trim_position <- max(x) - trim_bottom
-    df <- subset(df, x < bottom_trim_position)
-  }
-
-  # Replace all rows with validity == 0 with NA
-  if(na_validity==TRUE){
-
-    position_temp <- df$position
-
-    if(!is.null(depth_top)){
-      depth_temp <- df$depth
-    } else{}
-
-    df[df$validity == FALSE , ] <- NA
-
-    df$position <- position_temp
-
-    if(!is.null(depth_top)){
-      df$depth <- depth_temp
-    }
-  } else if(na_validity==FALSE){
+  # sort out bad data
+  if(is.numeric(depth_top) == TRUE){
+    df <- suppressMessages(full_join(df %>% filter(validity == TRUE),
+                                     df %>% filter(validity == FALSE) %>% select(any_of(c(elements, "position"))) %>% na_if(0))) %>%
+      arrange(position) %>%
+      mutate(depth = df$depth,
+             validity = df$validity,
+             cps = df$cps,
+             MSE = df$MSE) %>%
+      select(any_of("depth"), any_of(others[1:5]), any_of(elements) , any_of(others[6:length(others)]), everything())
   } else{
-    stop('The na_validity parameter must be TRUE or FALSE.')
+    df <- suppressMessages(full_join(df %>% filter(validity == TRUE),
+                                     df %>% filter(validity == FALSE) %>% select(any_of(c(elements, "position"))) %>% na_if(0))) %>%
+      arrange(position) %>%
+      mutate(validity = df$validity,
+             cps = df$cps,
+             MSE = df$MSE) %>%
+      select(any_of(others[1:5]), any_of(elements) , any_of(others[6:length(others)]), everything())
   }
-
-  # return the values as logic
-  df$validity = as.logical(df$validity)
-
-  # make a pretty graph
-
-  if(!is.null(depth_top)){
-    ylabel <- "Depth (mm)"
-  }else{ylabel <- "Position (mm)"}
-
-  if(graph==TRUE){
-    require(analogue)
-    if(!is.null(depth_top)){ analogue::Stratiplot(df[-grep("depth|position",colnames(df))], df$depth, varTypes = "absolute",
-                                        absolutesize = "1", strip = TRUE, ylab=as.character(ylabel)) }
-    else{	analogue::Stratiplot(df[-grep("depth|position",colnames(df))], df$position, varTypes = "absolute",
-                     absolutesize = "1", strip = TRUE, ylab=as.character(ylabel)) }
-  } else if(graph==FALSE){
+  # cut the ends
+  if((is.numeric(trim_top) && is.numeric(trim_bottom)) == TRUE){
+    df <- df %>% filter(position > min(position) + trim_top & position < max(position) - trim_bottom)
   } else{
-    stop('The graph parameter must be TRUE or FALSE.')
+    stop("trim_bottom and trim_top should be numeric - set to 0 if no trim required")
   }
 
-  # export the data
-  if(!is.null(export)){
-    write.table(df, file = export, sep = "\t")
-  } else{}
-
-  # return the data
+  # tidy up
   return(df)
 }
