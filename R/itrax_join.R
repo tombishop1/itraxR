3e188a8622f7fe57758cb3e8f7fbc81f39ab530d
tombishop1 <tombishopemail@gmail.com>
2020-11-09 16:20:38 +0000

    Update itrax_join function
    The old itrax_join function is rewritten to have better error messaging. and clearer code.

--- R/itrax_join.R
+++ R/itrax_join.R
@@ -2,34 +2,22 @@
 #'
 #' Join two or more Itrax datasets that have been parsed using \code{"itrax_import()"}
 #'
-#' @param list a list of dataframes that are parsed Itrax result files --- this should have been imported using \code{"itrax_import()"} and must have a depth variable present
-#' #'
-#' @return a dataframe of all the input data
+#' @param list a list of dataframes that are parsed Itrax result files --- this should have been imported using \code{"itrax_import()"} and must have a depth variable present. This should take the form \code{"list(core1 = core1, core2 = core2)"}
 #'
-#' @examples
-#' itrax_join( list(core1 = df1, core2 = df2, core3 = df3) )
+#' @return a tibble of all the input data
 #'
 #' @export
 
-# DEPTH MUST BE PRESENT
-# list should be in the format "list(core1 = dataframe1, core 2 = dataframe2, etc...)"
-
-# label the individual scans - this needs to iterate along the list called "list", e.g.
-# dataframe1$labels <- deparse(substitute(dataframe1))
-# dataframe2$labels <- deparse(substitute(dataframe2)) etc. etc.
-
-itrax_join=function(list){
-
+itrax_join <- function(list){
   require(dplyr)
 
-  # label the data
-  list <- lapply(names(list), function(i) within(list[[i]], {label <- i}))
-
-  # join them
-  df <- dplyr::bind_rows(list)
-
-  # sort them by depth
-  df <- df[with( df, order(depth) ) , ]
+  if(all(unlist(lapply(list, function(x) "depth" %in% colnames(x)))) == FALSE){
+    stop("Depth variable is required when joining core sections, but is missing from one or more sections.")
+  } else{
+    df <- lapply(names(list), function(i) within(list[[i]], {label <- i})) %>%
+      bind_rows() %>%
+      arrange(depth)
+  }
 
   return(df)
 }
