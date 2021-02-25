f75382c6f4b6016dcbed5461f2073f4ee0060916
tombishop1 <tombishopemail@gmail.com>
2021-02-25 12:55:56 +0000

    Add plotting options
    Adds side-plots to itrax_ordination() and itrax_section() for quick data review, diagnostics

--- R/itrax_ordination.r
+++ R/itrax_ordination.r
@@ -7,11 +7,14 @@
 #' @param zeros if "addone", adds one to all values. If "limit", replaces zero values with 0.001. Otherwise a function can be supplied to remove zero values.
 #' @param transform binary operator that if TRUE will center-log-transform the data, if FALSE will leave the data untransformed. Otherwise, a function can be supplied to transform the data.
 #' @param return if "pca" the output of \code{prcomp()} is returned, otherwise "list" is a list including the transformed data, sample scores, and the output of prcomp().
+#' @param plot set to true if a biplot is required as a side-effect
 #'
 #' @importFrom tidyr drop_na
 #' @importFrom stats prcomp
 #' @importFrom compositions clr
 #' @importFrom rlang .data
+#' @importFrom broom augment
+#' @import ggfortify
 #'
 #' @return either an output of \code{prcomp()}, or a list including the input data
 #'
@@ -21,10 +24,14 @@
 #' @export
 
 itrax_ordination <- function(dataframe,
-                                 elementsonly = TRUE,
-                                 zeros = "addone",
-                                 transform = TRUE,
-                                 return = "list"){
+                             elementsonly = TRUE,
+                             zeros = "addone",
+                             transform = TRUE,
+                             return = "list",
+                             plot = FALSE){
+
+  # fudge to stop build notes
+  ids <- NULL
 
   # label with ids
   dataframe$ids <- 1:dim(dataframe)[1]
@@ -75,6 +82,15 @@ itrax_ordination <- function(dataframe,
     scale() %>%
     prcomp()
 
+  # make a plot if required
+  if(is.logical(plot) == TRUE && plot == TRUE){
+  print(autoplot(pca,
+           loadings = TRUE,
+           loadings.label = TRUE))
+  } else if(is.logical(plot) == FALSE){
+    stop("plot parameter must be logical (TRUE/FALSE)")
+  }
+
   # select the return
   if(return == "pca"){
     #warning("`return = \"pca\"` will be depreciated, but for now is retained for compatibility.") # not sure about this yet
@@ -95,7 +111,3 @@ itrax_ordination <- function(dataframe,
                   )
              )
 }
-
-# TODO make plotting function here
-# myPCA %>%
-#  broom::augment(CD166_19_S1$xrf %>% tidyr::drop_na())
--- R/itrax_section.R
+++ R/itrax_section.R
@@ -1,12 +1,15 @@
 #' Cluster analysis and statistical grouping of Itrax data
 #'
-#' Performs a cluster analysis and automatic statistical grouping of parsed Itrax results data
+#' Performs a cluster analysis and automatic statistical grouping of parsed Itrax results data to n groups.
+#' Also provides information on the most "representative" (central) of each group. These can be used to develop a
+#' sub-sampling regime for calibration using another method.
 #'
 #' @param dataframe pass the name of a dataframe parsed using \code{"itrax_import()"} or \code{"itrax_join()"}
 #' @param elementsonly if TRUE, only chemical elements are included. If FALSE, the data is passed unfiltered, otherwise a character vector of desired variable names can be supplied
 #' @param zeros if "addone", adds one to all values. If "limit", replaces zero values with 0.001. Otherwise a function can be supplied to remove zero values.
 #' @param transform binary operator that if TRUE will center-log-transform the data, if FALSE will leave the data untransformed. Otherwise, a function can be supplied to transform the data.
-#' @param divisions the number of samples to slice into
+#' @param divisions the number of groups to slice into - also the number of representative samples returned
+#' @param plot set to true if a summary plot is required as a side-effect - the input dataset must have a depth or position variable - depth is used preferentially.
 #'
 #' @importFrom tidyr drop_na
 #' @importFrom stats prcomp hclust dist cutree
@@ -22,15 +25,17 @@
 #'
 
 itrax_section <- function(dataframe,
-                              divisions = 30,
-                             elementsonly = TRUE,
-                             zeros = "addone",
-                             transform = TRUE){
+                          divisions = 30,
+                          elementsonly = TRUE,
+                          zeros = "addone",
+                          transform = TRUE,
+                          plot = FALSE){
 
   # fudge to stop check notes
   . = NULL
   group = NULL
   ids = NULL
+  position = NULL
 
   # label with ids
   dataframe$ids <- 1:dim(dataframe)[1]
@@ -77,8 +82,6 @@ itrax_section <- function(dataframe,
     dataframe <- transform(dataframe)
   }
 
-  #glimpse(dataframe)
-
   # perform the first ordering
   firstorder <- as_tibble(dataframe) %>%
     mutate(group = dataframe %>%
@@ -90,10 +93,8 @@ itrax_section <- function(dataframe,
     select(`ids`, `group`)
 
   # perform second ordering
-  # subset a second order group
-
-  rep_samples <- lapply(unique(firstorder$group), function(x){x
-
+  rep_samples <- lapply(unique(firstorder$group), function(x){
+    # subset a second order group
   second_order_subset <- as_tibble(dataframe) %>%
     mutate(group = firstorder$group) %>%
     filter(group == x)
@@ -114,13 +115,46 @@ itrax_section <- function(dataframe,
   rep_samples <- rep_samples %>%
     unlist()
 
-  #glimpse(rep_samples)
-
   rep_samples <- input_dataframe %>%
     filter(`ids` %in% rep_samples) %>%
     select(-`ids`)
 
-  #glimpse(rep_samples)
+  # do a plot if required
+  if(is.logical(plot) == TRUE && plot == TRUE){
+    if("depth" %in% colnames(rep_samples) == TRUE){
+    print(ggplot() +
+            geom_bar(data = right_join(firstorder, input_dataframe, by = "ids"),
+                     aes(x = depth, fill = as.factor(group)),
+                     width = 1) +
+            geom_point(data = rep_samples,
+                       aes(x = depth),
+                       y = 0.5,
+                       shape = 3) +
+            scale_x_reverse() +
+            theme(axis.title.y = element_blank(),
+                  axis.text.y = element_blank(),
+                  axis.ticks.y = element_blank(),
+                  legend.position = "none")
+          )
+    } else if("depth" %in% colnames(rep_samples) == FALSE && "position" %in% colnames(rep_samples) == TRUE){
+      print(ggplot() +
+              geom_bar(data = right_join(firstorder, input_dataframe, by = "ids"),
+                       aes(x = position, fill = as.factor(group)),
+                       width = 1) +
+              geom_point(data = rep_samples,
+                         aes(x = position),
+                         y = 0.5,
+                         shape = 3) +
+              scale_x_reverse() +
+              theme(axis.title.y = element_blank(),
+                    axis.text.y = element_blank(),
+                    axis.ticks.y = element_blank(),
+                    legend.position = "none")
+            )
+      } else(stop("if plot = TRUE, you must include either a depth or position parameter"))
+  } else if(is.logical(plot) == FALSE){
+    stop("plot parameter must be logical (TRUE/FALSE)")
+  }
 
   # sort the return
   return(list(groups = right_join(firstorder, input_dataframe, by = "ids") %>%
@@ -128,6 +162,4 @@ itrax_section <- function(dataframe,
               samples = rep_samples))
 }
 
-# TODO make plotting function here
-# myPCA %>%
-#  broom::augment(CD166_19_S1$xrf %>% tidyr::drop_na())
+
