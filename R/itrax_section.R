be6e28a0d00c2f2495807afe1546e93118f794a6
tombishop1 <tombishopemail@gmail.com>
2022-04-07 21:49:45 +0100

    Makes `itrax_section` more robust

--- R/itrax_section.R
+++ R/itrax_section.R
@@ -20,6 +20,7 @@
 #'
 #' @examples
 #' itrax_section(CD166_19_S1$xrf, plot = TRUE)
+#' itrax_section(CD166_19_S1$xrf %>% itrax_reduce(by = 10), plot = TRUE)
 #'
 #' @export
 #'
@@ -37,99 +38,113 @@ itrax_section <- function(dataframe,
   . <- NULL
   uid <- NULL
 
-# copy the original data
-original_data <- dataframe
-n <- divisions
+  # copy the original data
+  original_data <- dataframe
+  n <- divisions
 
-# label it with ids
-# if there are already uids, and they are unique, it will just use them
-original_data <- uid_labeller(original_data)
+  # label it with ids
+  # if there are already uids, and they are unique, it will just use them
+  original_data <- uid_labeller(original_data)
 
-# make a transformed version of the data
-transformed_data <- original_data %>%
-  multivariate_import(elementsonly = elementsonly,
-                      zeros = zeros,
-                      transform = transform) %>%
-  compositions::clr()
+  # make a transformed version of the data
+  transformed_data <- original_data %>%
+    multivariate_import(elementsonly = elementsonly,
+                        zeros = zeros,
+                        transform = transform) %>%
+    compositions::clr()
 
-# perform first pass work
-firstgroups <- left_join(original_data,
-                         tibble(uid = transformed_data %>%
-                                  rownames(),
-                                group = dist(transformed_data) %>%
-                                  hclust(method = "ward.D2") %>%
-                                  cutree(k = n) %>%
-                                  as.factor()
-                                ),
-                         by = "uid")
+  # perform first pass work
+  firstgroups <- left_join(original_data,
+                           tibble(uid = transformed_data %>%
+                                    rownames(),
+                                  group = dist(transformed_data) %>%
+                                    hclust(method = "ward.D2") %>%
+                                    cutree(k = n) %>%
+                                    as.factor()
+                           ),
+                           by = "uid")
 
-# perform second pass work - start by splitting
-split_groups <- firstgroups %>%
-  select(.data$uid, .data$group) %>%
-  tidyr::drop_na() %>%
-  group_by(.data$group) %>%
-  group_split()
+  # perform second pass work - start by splitting
+  split_groups_full <- firstgroups %>%
+    select(.data$uid, .data$group) %>%
+    tidyr::drop_na() %>%
+    group_by(.data$group) %>%
+    group_split()
 
-samples <- lapply(na.omit(unique(firstgroups$group)),
-                  function(x){
-                    transformed_data[split_groups[[x]] %>% pull(.data$uid),] %>%
-                      dist() %>%
-                      hclust(method = "ward.D2") %>%
-                      `[[`("labels") %>%
-                      `[[`(length(.)/2)
+  # we need to remove any groups with only one observation in, becuase they are going to fail on hclust()
+  # but first check this is the situation
+  if( !length(split_groups_full[ which(lapply(split_groups_full, nrow) %>% unlist() == 1) ]) == 0){
+    split_groups <- split_groups_full[ -which(lapply(split_groups_full, nrow) %>% unlist() == 1) ]
+  } else{split_groups <- split_groups_full}
+
+  # now do the second pass grouping
+  samples <- lapply(seq(from = 1, to = length(split_groups)),
+                    function(x){
+                      transformed_data[split_groups[[x]] %>% pull(.data$uid),] %>%
+                        dist() %>%
+                        hclust(method = "ward.D2") %>%
+                        `[[`("labels") %>%
+                        `[[`(length(.)/2)
                     }) %>%
-  unlist() %>%
-  as_tibble() %>%
-  rename(uid = .data$value) %>%
-  mutate(calib_sample = TRUE)
+    unlist() %>%
+    as_tibble() %>%
+    rename(uid = .data$value) %>%
+    mutate(calib_sample = TRUE)
 
-# deal with the special case of groups with only one observation
-# it identifies groups with only one observation
-# appends them to the regular list
-# then deletes the weird values generated previously from groups of only one observation
-samples <- full_join(samples, 
-                     tibble(uid = split_groups[ which(lapply(split_groups, nrow) %>% unlist() == 1) ] %>%
-                              lapply(., function(x){select(x, "uid")}) %>%
-                              unlist(),
-                            calib_sample = TRUE),
-                     by = c("uid", "calib_sample")) %>%
-  filter() %>%
-  mutate(uid = as.character(uid)) %>%
-  drop_na()
+  # deal with the special case of groups with only one observation
+  # it identifies groups with only one observation
+  # appends them to the regular list
+  # then deletes the weird values generated previously from groups of only one observation
+  # but first check if that even needs to be done
 
-# create the output object
-df <- left_join(firstgroups, samples, by = "uid") %>%
-  tidyr::replace_na(list(calib_sample = FALSE))
+  if( !length(split_groups_full[ which(lapply(split_groups_full, nrow) %>% unlist() == 1) ]) == 0){
 
-# sideplot if required
-if(plot == TRUE && !"depth" %in% colnames(df)){
-  print(
-    df %>%
-      ggplot(aes(x = .data$position, y = 1, fill = .data$group)) +
-      geom_tile() +
-      scale_x_reverse() +
-      theme(axis.title.y = element_blank(),
-            axis.text.y = element_blank(),
-            axis.ticks.y = element_blank(),
-            legend.position = "none") +
-      geom_rug(sides = "b", data = filter(df, calib_sample == TRUE))
-  )
+    samples <- suppressMessages(
+      full_join(samples,
+                tibble(uid = split_groups_full[ which(lapply(split_groups_full, nrow) %>% unlist() == 1) ] %>%
+                         lapply(., function(x){select(x, "uid")}) %>%
+                         unlist(),
+                       calib_sample = TRUE)#,
+                #by = c("uid", "calib_sample")
+      )
+    ) %>%
+      filter() %>%
+      mutate(uid = as.character(uid))
+  }
 
-} else if(plot == TRUE && "depth" %in% colnames(df)){
-  print(
-    df %>%
-      ggplot(aes(x = .data$depth, y = 1, fill = .data$group)) +
-      geom_tile() +
-      scale_x_reverse() +
-      theme(axis.title.y = element_blank(),
-            axis.text.y = element_blank(),
-            axis.ticks.y = element_blank(),
-            legend.position = "none") +
-      geom_rug(sides = "b", data = filter(df, calib_sample == TRUE))
-  )
+  # create the output object
+  df <- left_join(firstgroups, samples, by = "uid") %>%
+    tidyr::replace_na(list(calib_sample = FALSE))
+
+  # sideplot if required
+  if(plot == TRUE && !"depth" %in% colnames(df)){
+    print(
+      df %>%
+        ggplot(aes(x = .data$position, y = 1, fill = .data$group)) +
+        geom_tile() +
+        scale_x_reverse() +
+        theme(axis.title.y = element_blank(),
+              axis.text.y = element_blank(),
+              axis.ticks.y = element_blank(),
+              legend.position = "none") +
+        geom_rug(sides = "b", data = filter(df, calib_sample == TRUE))
+    )
+
+  } else if(plot == TRUE && "depth" %in% colnames(df)){
+    print(
+      df %>%
+        ggplot(aes(x = .data$depth, y = 1, fill = .data$group)) +
+        geom_tile() +
+        scale_x_reverse() +
+        theme(axis.title.y = element_blank(),
+              axis.text.y = element_blank(),
+              axis.ticks.y = element_blank(),
+              legend.position = "none") +
+        geom_rug(sides = "b", data = filter(df, calib_sample == TRUE))
+    )
 
   }
 
-# create the output
-return(df)
+  # create the output
+  return(df)
 }
