1f7221a990e17e31a57855beb454a8f44c5c1f3b
tombishop1 <tombishopemail@gmail.com>
2022-12-31 17:50:37 +0000

    `itrax_restspectra()` reads from zip
    Adds the ability to read spectral files directly from the .zip.

--- R/itrax_restspectra.r
+++ R/itrax_restspectra.r
@@ -2,7 +2,7 @@
 #'
 #' Parses a folder full of raw spectra files from an Itrax core scanner and produces a spectral graph of all the data by position
 #'
-#' @param foldername defines the folder where the spectra \code{"*.spe"} files are located
+#' @param foldername defines the folder where the spectra \code{"*.spe"} files are located - or the path of the zipped folder where it is stored.
 #' @param parameters optionally, defines the Q-Spec settings file from which to calculate the channel energies
 #' @param datapos defines the row at which spectral data begins in the files
 #' @param plot TRUE/FALSE, selects whether to create a plot as a side-effect
@@ -16,6 +16,7 @@
 #' @import dplyr ggplot2 readr
 #' @importFrom rlang .data
 #' @importFrom stringr str_which
+#' @importFrom utils unzip
 #'
 #'
 #' @export
@@ -23,7 +24,7 @@
 # function for integrating raw xrf spectra and visualising the same
 itrax_restspectra <- function(foldername = "XRF data",
                               parameters = "settings.dfl",
-                              datapos=37,
+                              datapos = 37,
                               plot = TRUE,
                               trans = "pseudo_log") {
 
@@ -41,23 +42,46 @@ itrax_restspectra <- function(foldername = "XRF data",
     channel_offset <- as.numeric(pull(settings[stringr::str_which(settings$key, pattern = "energy offset"), "value"]))
     rm(settings)}
 
-  # read in a list of files
-  filenames <- dir(foldername, pattern="*.spe")
-  filenames <- paste(foldername, filenames, sep="/")
+  if(dir.exists(foldername)){
+    # read in a list of files
+    filenames <- dir(foldername, pattern="*.spe")
+    filenames <- paste(foldername, filenames, sep="/")
+  } else if(file.exists(foldername)){
+    filenames <- unzip(zipfile = foldername, list = TRUE)$Name
+  } else{stop("The folder or *.zip file doesn't exist.")}
 
   # import spectral data
-  tables <- suppressWarnings(lapply(filenames, 
-                                    itrax_spectra, 
-                                    plot = FALSE)
-                             )
+  if(dir.exists(foldername)){
+    tables <- suppressWarnings(lapply(filenames,
+                                      itrax_spectra,
+                                      plot = FALSE)
+    )
+  } else if(file.exists(foldername)){
+    tables <- lapply(filenames,
+                     function(x){itrax_spectra(unz(description = foldername,
+                                                   filename = x),
+                                               plot = FALSE)
+                     }
+    )
+  }
 
   # import all the scan positions
-  depths <- suppressWarnings(lapply(filenames,
-                                    read.table,
-                                    skip = 6,
-                                    nrows = 1,
-                                    header = FALSE)
-                             )
+  if(dir.exists(foldername)){
+    depths <- suppressWarnings(lapply(filenames,
+                                      read.table,
+                                      skip = 6,
+                                      nrows = 1,
+                                      header = FALSE)
+    )
+  } else if(file.exists(foldername)){
+    depths <- lapply(filenames,
+                     function(x){read.table(unz(description = foldername,
+                                                filename = x),
+                                            skip = 6,
+                                            nrows = 1,
+                                            header = FALSE)}
+    )
+  }
 
   # label all the dataframes in the list
   names(tables) <- unname(sapply(depths, `[[`, 2))
@@ -86,32 +110,32 @@ itrax_restspectra <- function(foldername = "XRF data",
 
   # draw a plot as a side-effect if required
   if(plot == TRUE){
-  p <- foo %>%
-    tidyr::pivot_longer(cols = c(-filename, -position)) %>%
-    mutate(name = as.numeric(name)) %>%
-    rename(channel = name) %>%
-    select(-filename) %>%
+    p <- foo %>%
+      tidyr::pivot_longer(cols = c(-filename, -position)) %>%
+      mutate(name = as.numeric(name)) %>%
+      rename(channel = name) %>%
+      select(-filename) %>%
 
-    ggplot(aes(x = channel, y = position, fill = value)) +
-    geom_raster() +
-    scale_fill_gradient(name = "value",
-                        trans = trans,
-                        low = "#132B43",
-                        high = "#56B1F7",
-                        labels = round) +
-    scale_y_reverse() +
-    ylab("position [mm]") +
-    guides(fill = "none")
+      ggplot(aes(x = channel, y = position, fill = value)) +
+      geom_raster() +
+      scale_fill_gradient(name = "value",
+                          trans = trans,
+                          low = "#132B43",
+                          high = "#56B1F7",
+                          labels = round) +
+      scale_y_reverse() +
+      ylab("position [mm]") +
+      guides(fill = "none")
 
-  if(exists("channel_offset") && exists("channel_kev")){
-    p <- p + xlab("energy [k eV]") +
-      scale_x_continuous(sec.axis = sec_axis(trans = ~ (. / channel_kev) - channel_offset,
-                                             name = "channel [n]"))
-  } else{
-    p <- p + xlab("channel [n]")
-  }
+    if(exists("channel_offset") && exists("channel_kev")){
+      p <- p + xlab("energy [k eV]") +
+        scale_x_continuous(sec.axis = sec_axis(trans = ~ (. / channel_kev) - channel_offset,
+                                               name = "channel [n]"))
+    } else{
+      p <- p + xlab("channel [n]")
+    }
 
-  print(p)
+    print(p)
 
   }
 
